7a09e301c609569ad4510b8f069271ae39159a9b
Davit2023 <choundavit11@gmail.com>
2023-08-09 23:45:25 +0700

    disk

---
+++ node_modules/.cache/nuxt/dist/server/pages/course.js
@@ -0,0 +1,164 @@
+exports.ids = [5];
+exports.modules = {
+
+/***/ 51:
+/***/ (function(module, exports, __webpack_require__) {
+
+// style-loader: Adds some css to the DOM by adding a <style> tag
+
+// load the styles
+var content = __webpack_require__(73);
+if(content.__esModule) content = content.default;
+if(typeof content === 'string') content = [[module.i, content, '']];
+if(content.locals) module.exports = content.locals;
+// add CSS to SSR context
+var add = __webpack_require__(4).default
+module.exports.__inject__ = function (context) {
+  add("b49eddac", content, true, context)
+};
+
+/***/ }),
+
+/***/ 66:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/jett.96e1aac.jpg";
+
+/***/ }),
+
+/***/ 67:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/wp5159584.7ed4f31.webp";
+
+/***/ }),
+
+/***/ 68:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/sun-monkey-king-mobile-legends-uhdpaper.com-4K-154.3862197.webp";
+
+/***/ }),
+
+/***/ 69:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/wp4527624.db30770.webp";
+
+/***/ }),
+
+/***/ 70:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/V4XAn9c.690cc18.webp";
+
+/***/ }),
+
+/***/ 71:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/Mobile-Legends-Hayabusa-epic-skin.7944c64.webp";
+
+/***/ }),
+
+/***/ 72:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_3_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_course_vue_vue_type_style_index_0_id_864346d0_prod_lang_css___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(51);
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_3_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_course_vue_vue_type_style_index_0_id_864346d0_prod_lang_css___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_3_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_course_vue_vue_type_style_index_0_id_864346d0_prod_lang_css___WEBPACK_IMPORTED_MODULE_0__);
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_3_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_course_vue_vue_type_style_index_0_id_864346d0_prod_lang_css___WEBPACK_IMPORTED_MODULE_0__) if(["default"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_3_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_course_vue_vue_type_style_index_0_id_864346d0_prod_lang_css___WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+
+
+/***/ }),
+
+/***/ 73:
+/***/ (function(module, exports, __webpack_require__) {
+
+// Imports
+var ___CSS_LOADER_API_IMPORT___ = __webpack_require__(3);
+var ___CSS_LOADER_EXPORT___ = ___CSS_LOADER_API_IMPORT___(function(i){return i[1]});
+// Module
+___CSS_LOADER_EXPORT___.push([module.i, ".course-head{font-weight:700}.box{height:205px;margin-top:-9px;overflow:hidden;width:100%}.box-img{height:100%!important;transition:all .19s ease;width:100%!important}.my_course{background-color:#0f0f0f;border-radius:10px;overflow:hidden}.box-img:hover{background-color:#0f0f0f;transform:scale(1.2)}", ""]);
+// Exports
+___CSS_LOADER_EXPORT___.locals = {};
+module.exports = ___CSS_LOADER_EXPORT___;
+
+
+/***/ }),
+
+/***/ 95:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/course.vue?vue&type=template&id=864346d0&
+var render = function render() {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c('div', [_vm._ssrNode("<div class=\"section-content\"><div class=\"full-height text-center\"><h3 class=\"pt-4 mt-5 text-gray text-uppercase course-head\">My Courses</h3> <div class=\"container mt-2\" style=\"font-size: 20px;\"><div class=\"container\"><p data-aos=\"fade-up\" data-aos-duration=\"3000\" class=\"text-gray-400 text-xl\">This is the website portfolio\n            about\n            me for\n            sharing experiences with everyone who is best on technology coding 2023 . Thank Yous</p></div></div> <div class=\"container mt-5\" style=\"padding-bottom: 80px;\"><div class=\"row pt-4\"><div data-aos=\"fade-left\" data-aos-delay=\"300\" class=\"col-lg-4 col-md-4 col-sm-4 col-12 p-2\"><div class=\"my_course\"><div class=\"box\"><img" + _vm._ssrAttr("src", __webpack_require__(66)) + " alt class=\"image-icon lg mb-2 box-img\"></div> <h3 class=\"text-white mt-3\">Backend Developer</h3> <p class=\"p-1\">\n                Lorem ipsum dolor, sit amet consectetur adipisicing elit.\n                At, rerum in eos ducimus ea nulla asperiores, odit vitae\n              </p></div></div> <div data-aos=\"fade-left \" data-aos-delay=\"500\" class=\"col-lg-4 col-md-4 col-sm-4 col-12 p-2\"><div class=\"my_course\"><div class=\"box\"><img" + _vm._ssrAttr("src", __webpack_require__(67)) + " alt class=\"image-icon lg mb-2 box-img\"></div> <h3 class=\"text-white mt-3\">Frontend Developer</h3> <p class=\"p-1\">\n                Lorem ipsum dolor, sit amet consectetur adipisicing elit.\n                At, rerum in eos ducimus ea nulla asperiores, odit vitae\n              </p></div></div> <div data-aos=\"fade-left\" data-aos-delay=\"700\" class=\"col-lg-4 col-md-4 col-sm-4 col-12 p-2\"><div class=\"my_course\"><div class=\"box\"><img" + _vm._ssrAttr("src", __webpack_require__(68)) + " alt class=\"image-icon lg mb-2 box-img\"></div> <h3 class=\"text-white mt-3\">Fullstack Developer</h3> <p class=\"p-1\">\n                Lorem ipsum dolor, sit amet consectetur adipisicing elit.\n                At, rerum in eos ducimus ea nulla asperiores, odit vitae\n              </p></div></div> <div data-aos=\"fade-left\" data-aos-delay=\"900\" class=\"col-lg-4 col-md-4 col-sm-4 col-12 p-2\"><div class=\"my_course\"><div class=\"box\"><img" + _vm._ssrAttr("src", __webpack_require__(69)) + " alt class=\"image-icon lg mb-2 box-img\"></div> <h3 class=\"text-white mt-3\">Backend Developer</h3> <p class=\"p-1\">\n                Lorem ipsum dolor, sit amet consectetur adipisicing elit.\n                At, rerum in eos ducimus ea nulla asperiores, odit vitae\n              </p></div></div> <div data-aos=\"fade-left \" data-aos-delay=\"1100\" class=\"col-lg-4 col-md-4 col-sm-4 col-12 p-2\"><div class=\"my_course\"><div class=\"box\"><img" + _vm._ssrAttr("src", __webpack_require__(70)) + " alt class=\"image-icon lg mb-2 box-img\"></div> <h3 class=\"text-white mt-3\">Frontend Developer</h3> <p class=\"p-1\">\n                Lorem ipsum dolor, sit amet consectetur adipisicing elit.\n                At, rerum in eos ducimus ea nulla asperiores, odit vitae\n              </p></div></div> <div data-aos=\"fade-left\" data-aos-delay=\"1300\" class=\"col-lg-4 col-md-4 col-sm-4 col-12 p-2\"><div class=\"my_course\"><div class=\"box\"><img" + _vm._ssrAttr("src", __webpack_require__(71)) + " alt class=\"image-icon lg mb-2 box-img\"></div> <h3 class=\"text-white mt-3\">Fullstack Developer</h3> <p class=\"p-1\">\n                Lorem ipsum dolor, sit amet consectetur adipisicing elit.\n                At, rerum in eos ducimus ea nulla asperiores, odit vitae\n              </p></div></div></div></div></div></div>")]);
+};
+var staticRenderFns = [];
+
+// CONCATENATED MODULE: ./pages/course.vue?vue&type=template&id=864346d0&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/course.vue?vue&type=script&lang=js&
+/* harmony default export */ var coursevue_type_script_lang_js_ = ({
+  name: "IndexPage",
+  layout: "App",
+  data() {
+    return {
+      items: []
+    };
+  },
+  created() {
+    this.getuser();
+  },
+  mounted() {},
+  methods: {
+    async getuser() {
+      let res = await this.$axios.post('/get_course').then(res => {
+        this.items = res.data;
+        console.log(res.data);
+      });
+    }
+  }
+});
+// CONCATENATED MODULE: ./pages/course.vue?vue&type=script&lang=js&
+ /* harmony default export */ var pages_coursevue_type_script_lang_js_ = (coursevue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./pages/course.vue
+
+
+
+function injectStyles (context) {
+  
+  var style0 = __webpack_require__(72)
+if (style0.__inject__) style0.__inject__(context)
+
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pages_coursevue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  null,
+  "123bdc5b"
+  
+)
+
+/* harmony default export */ var course = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=course.js.map
---
+++ node_modules/.cache/nuxt/dist/server/pages/index.js
@@ -0,0 +1,150 @@
+exports.ids = [7];
+exports.modules = {
+
+/***/ 54:
+/***/ (function(module, exports, __webpack_require__) {
+
+// style-loader: Adds some css to the DOM by adding a <style> tag
+
+// load the styles
+var content = __webpack_require__(90);
+if(content.__esModule) content = content.default;
+if(typeof content === 'string') content = [[module.i, content, '']];
+if(content.locals) module.exports = content.locals;
+// add CSS to SSR context
+var add = __webpack_require__(4).default
+module.exports.__inject__ = function (context) {
+  add("3dc3a25e", content, true, context)
+};
+
+/***/ }),
+
+/***/ 84:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/back-end-developer-4316118-3611968.ec77ec6.webp";
+
+/***/ }),
+
+/***/ 85:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/full-stack-developer-working-on-project-5248497-4403356.717a6c6.webp";
+
+/***/ }),
+
+/***/ 86:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/web-development.924aefc.png";
+
+/***/ }),
+
+/***/ 87:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/backend-icon-24.a952cac.png";
+
+/***/ }),
+
+/***/ 88:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/ux.ffa3e0d.png";
+
+/***/ }),
+
+/***/ 89:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_02642831_prod_lang_scss___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(54);
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_02642831_prod_lang_scss___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_02642831_prod_lang_scss___WEBPACK_IMPORTED_MODULE_0__);
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_02642831_prod_lang_scss___WEBPACK_IMPORTED_MODULE_0__) if(["default"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_02642831_prod_lang_scss___WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+
+
+/***/ }),
+
+/***/ 90:
+/***/ (function(module, exports, __webpack_require__) {
+
+// Imports
+var ___CSS_LOADER_API_IMPORT___ = __webpack_require__(3);
+var ___CSS_LOADER_EXPORT___ = ___CSS_LOADER_API_IMPORT___(function(i){return i[1]});
+// Module
+___CSS_LOADER_EXPORT___.push([module.i, ".colors-text{background:linear-gradient(45deg,blue,#ff2f52);-webkit-background-clip:text;color:transparent}@media screen and (max-width:559px){.colors-text{font-size:2rem!important}}@media screen and (max-width:390px){.colors-text{font-size:1.9rem!important}}.personal{background:linear-gradient(45deg,#ff2f52,#3fff3f);-webkit-background-clip:text;color:transparent}@media screen and (max-width:559px){.personal{font-size:2rem!important}}@media screen and (max-width:390px){.personal{font-size:1.9rem!important}}.interest{background:linear-gradient(45deg,#ffef0f,#1e00e3,#ff7300);-webkit-background-clip:text;color:transparent}@media screen and (max-width:559px){.interest{font-size:2rem!important}}@media screen and (max-width:390px){.interest{font-size:1.9rem!important}}.hovers{border-radius:8px;font-size:15px;transition:all .3s ease}.hovers:hover{transform:scale(1.1)}@keyframes animay{0%{transform:scale(.95)}50%{transform:scale(1)}to{transform:scale(.95)}}.web_interest{border-radius:10px;padding:10px 10px 10px 0;transition:all .3s ease}.web_interest:hover{box-shadow:0 0 40px 4px aqua;transform:scale(1.02)}#cotact{align-items:center;animation:animay 3s infinite;display:flex;justify-content:center}@media screen and (max-width:559px){#cotact img{margin-top:28px;width:345px}}@media screen and (max-width:390px){#cotact img{margin-top:28px;width:345px}}@media screen and (max-width:559px){.home_me{margin-top:-25px}}@media screen and (max-width:390px){.home_me{margin-top:-25px}}", ""]);
+// Exports
+___CSS_LOADER_EXPORT___.locals = {};
+module.exports = ___CSS_LOADER_EXPORT___;
+
+
+/***/ }),
+
+/***/ 99:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/index.vue?vue&type=template&id=02642831&
+var render = function render() {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c('div', [_vm._ssrNode("<div data-aos=\"fade-in\" data-aos-delay=\"300\" class=\"section-content\">", "</div>", [_vm._ssrNode("<div class=\"container pt-4 pb-4 home_me\">", "</div>", [_vm._ssrNode("<div class=\"row\" style=\"height: 100%;\">", "</div>", [_vm._ssrNode("<div class=\"col-sm-6 col-md-6 col-lg-6 pb-4 pt-4\">", "</div>", [_vm._ssrNode("<div>", "</div>", [_vm._ssrNode("<p>Hello welcome </p> <span><h1 id=\"me\" class=\"colors-text font-w-title\"><span>I'm</span> DAVIT CHOUN</h1></span> <h1>Fullstack Developer</h1> <p>\n              My name is Davit Choun, I'm a Full-Stack Web Developer . I have more than 3 years experience in\n              full-stack web developer. I develop websites with HTML, CSS, Bootstrap, JavaScript, Jquery, Vuejs, Nuxtjs,\n              Reactjs, Nodejs, UniApp, PHP,\n              Laravel and ThinkPhp .<br></p> "), _c('nuxt-link', {
+    staticClass: "btn primary-btn text-uppercase hovers",
+    attrs: {
+      "to": "/contact"
+    }
+  }, [_vm._v("\n              contact me\n            ")])], 2)]), _vm._ssrNode(" <div class=\"col-sm-6 col-md-6 col-lg-6\"><div id=\"cotact\"><img" + _vm._ssrAttr("src", __webpack_require__(84)) + " alt=\"pheak\"></div></div>")], 2)])]), _vm._ssrNode(" "), _vm._ssrNode("<div data-aos=\"fade-up\" data-aos-delay=\"600\" class=\"section-white\">", "</div>", [_vm._ssrNode("<div class=\"container pt-4 pb-4\">", "</div>", [_vm._ssrNode("<div class=\"sec-paddingss\">", "</div>", [_vm._ssrNode("<div class=\"row\">", "</div>", [_vm._ssrNode("<div class=\"col-sm-6\"><img" + _vm._ssrAttr("src", __webpack_require__(85)) + " width=\"500px\" id=\"cotact\" alt=\"pheak\" class=\"single-image\"></div> "), _vm._ssrNode("<div class=\"col-sm-6\">", "</div>", [_vm._ssrNode("<p class=\"text-uppercase\">About Me</p> <h1 class=\"text-uppercase font-w-title personal\">\n              Personal details\n            </h1> <p>\n              My name is Choun Davit is a Cambodian Professional Backend Develop &amp; Frontend Develop.\n              I grew up in Takeo province,I attended 10 January 2012 High School, graduating in the class of 2022. I\n              received mY bachelor's degree in Computer Science​ and Engineer at RUPP.I started my career at phnom penh,\n              worked for 1 years on web developer. I then worked for 1 year at CodeWriterKh as Backend Developer, after\n              I’d start worked for 1 years at Onesala Co., Ltd as Backend Developer &amp; Web , and Freelance Developer.\n              </p> "), _c('nuxt-link', {
+    staticClass: "btn primary-btn text-uppercase hovers",
+    attrs: {
+      "to": "/about"
+    }
+  }, [_vm._v("About me")])], 2)], 2)])])]), _vm._ssrNode(" <div data-aos=\"fade-up\" data-aos-delay=\"300\" class=\"section-content\"><div class=\"container\"><div class=\"c\"><div class=\"row\"><div class=\"col-sm-12 text-center\"><h1 class=\"text-uppercase font-w-header interest mt-3\">\n              interest Content\n            </h1> <div class=\"row pt-4 mb-3\"><div data-aos=\"fade-left \" data-aos-delay=\"500\" class=\"col-12 col-sm-4 col-md-4 col-lg-4\"><div class=\"web_interest\"><img" + _vm._ssrAttr("src", __webpack_require__(86)) + " alt class=\"image-icon lg mb-2\"> <h3 class=\"text-white\">Web Develop</h3> <p>\n                    Today, the online experience is essential to the success of every business and organization. Without\n                    a web developer, companies are left using drag-and-drop web builders like Squarespace or a WordPress\n                    template.\n                  </p></div></div> <div data-aos=\"fade-left\" data-aos-delay=\"700\" class=\"col-12 col-sm-4 col-md-4 col-lg-4\"><div class=\"web_interest\"><img" + _vm._ssrAttr("src", __webpack_require__(87)) + " alt class=\"image-icon lg mb-2\"> <h3 class=\"text-white\">BackEnd Developer</h3> <p>\n                    A back-end web developer is responsible for server-side web application logic and integration of the\n                    work front-end web developers do.\n                  </p></div></div> <div data-aos=\"fade-left\" data-aos-delay=\"900\" class=\"col-12 col-sm-4 col-md-4 col-lg-4\"><div class=\"web_interest\"><img" + _vm._ssrAttr("src", __webpack_require__(88)) + " alt class=\"image-icon lg mb-2\"> <h3 class=\"text-white\">Frontend Developer</h3> <p>\n                    A front-end developer is a type of software developer who specializes in creating and designing the\n                    user interface (UI) and user experience (UX) of websites and web applications.\n                  </p></div></div></div></div> <div class=\"col-sm-6\"></div></div></div></div></div>")], 2);
+};
+var staticRenderFns = [];
+
+// CONCATENATED MODULE: ./pages/index.vue?vue&type=template&id=02642831&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/index.vue?vue&type=script&lang=js&
+/* harmony default export */ var lib_vue_loader_options_pagesvue_type_script_lang_js_ = ({
+  name: "IndexPage",
+  layout: "App"
+});
+// CONCATENATED MODULE: ./pages/index.vue?vue&type=script&lang=js&
+ /* harmony default export */ var pagesvue_type_script_lang_js_ = (lib_vue_loader_options_pagesvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./pages/index.vue
+
+
+
+function injectStyles (context) {
+  
+  var style0 = __webpack_require__(89)
+if (style0.__inject__) style0.__inject__(context)
+
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pagesvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  null,
+  "15ecce42"
+  
+)
+
+/* harmony default export */ var pages = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=index.js.map
